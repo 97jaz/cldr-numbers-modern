dbcc58eba923eb6a4f3c88858d270cac42349d53
Jon Zeppieri <jon.zeppieri@meyouhealth.com>
2015-03-22 22:17:05 -0400

    adds missing info.rkt files

---
+++ cldr/info.rkt
@@ -0,0 +1,3 @@
+#lang info
+
+(define scribblings '(("scribblings/cldr-numbers-modern.scrbl" ())))
---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+
+(define collection 'multi)
+(define deps '("base" "cldr-core"))
+(define build-deps '("scribble-lib" "racket-doc"))
+
+(define pkg-authors '(97jaz))
